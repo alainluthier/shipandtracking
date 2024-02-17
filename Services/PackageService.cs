b410102fcc9cff21f8ae64c365f9c4e7cd790cfb
zakerysattler@yahoo.com <zakerysattler@yahoo.com>
2024-02-17 17:32:10 -0500

    tracking

--- Services/PackageService.cs
+++ Services/PackageService.cs
@@ -6,26 +6,42 @@ using ShipAndTrack.Data;
 using Microsoft.EntityFrameworkCore;
 namespace ShipAndTrack.Services
 {
-    public class PackageService{
+    public class PackageService
+    {
         private readonly ShipAndTrackContext _context;
         //Creating the service with a database connection
-        public PackageService(ShipAndTrackContext context){
-            _context=context;
+        public PackageService(ShipAndTrackContext context)
+        {
+            _context = context;
         }
         //This async function will return a task with a list of packages by a Tracking number
-        public async Task<List<Package>> GetPackageByTrackingNumber(string track){
+        public async Task<List<Package>> GetPackageByTrackingNumber(string track)
+        {
             return await _context.Packages
             .Include(t => t.Trackings)
             .Include(u => u.User)
             .Include(a => a.Address)
-            .Where(x=> x.TrackingNumber==track)
+            .Where(x => x.TrackingNumber == track)
             .ToListAsync();
         }
         //This function will add a new Tracking status
-        public async Task<bool> AddTracking(Tracking tracking){
+        public async Task<bool> AddTracking(Tracking tracking)
+        {
             _context.Add(tracking);
             await _context.SaveChangesAsync();
             return true;
         }
+
+        public async Task<string> GetLatestTrackingStatusByTrackingNumber(string track)
+        {
+            var latestTracking = await _context.Packages
+                .Where(p => p.TrackingNumber == track)
+                .SelectMany(p => p.Trackings)
+                .OrderByDescending(t => t.CreatedAt)
+                .FirstOrDefaultAsync();
+
+            return latestTracking?.Status;
+        }
+
     }
 }
